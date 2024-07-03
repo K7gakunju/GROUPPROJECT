cfde0e30e5fe54b6fbc700aac0c06c6029d1ebfd
K7gakunju <165337404+K7gakunju@users.noreply.github.com>
2024-07-03 14:39:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+struct Student(){
+string firstname;
+string secondname;
+char gender;
 
+}
+void addstudent(){}
+void viewstudent(){}
+void viewclubs(){}
+void viewsports(){}
+void savefiles(){}
+void exit(){}
+
+int main(){
+
+}
