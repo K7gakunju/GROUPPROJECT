a3cb6b92693215038d9366f8497322802470109c
K7gakunju <165337404+K7gakunju@users.noreply.github.com>
2024-07-03 14:52:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,20 @@ struct Student(){
 string firstname;
 string secondname;
 char gender;
-
+string chosenclubs;
+string chosenclubs;
 }
+string firstname;
+string secondname;
+char gender;
 void addstudent(){}
 void viewstudent(){}
 void viewclubs(){}
 void viewsports(){}
 void savefiles(){}
 void exit(){}
-
 int main(){
+switch:
+ :
 
 }
